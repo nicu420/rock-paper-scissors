ab4333866acf4acd9b9379766fe5e1c3d025263f
nicu <ontanicolae15@gmail.com>
2022-08-18 17:47:35 +0300

    Implement playRound function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Generate a random choice
 function getComputerChoice() {
     let r = Math.floor(Math.random() * 3) + 1;
     switch (r) {
@@ -10,4 +11,40 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
+
+// Given 2 selections, check who won
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
+    if (player === computer) {
+        return "Tied";
+    }
+
+    switch (player) {
+        case "rock":
+            if (computer === "paper") {
+                return "You Lose! Paper beats Rock"
+            }
+            if (computer === "scissors") {
+                return "You Won! Rock beats Scissors"
+            }
+        case "paper":
+            if (computer === "scissors") {
+                return "You Lose! Scissors beats paper"
+            }
+            if (computer === "rock") {
+                return "You Won! Paper beats Rock"
+            }
+        case "scissors":
+            if (computer === "rock") {
+                return "You Lose! Rock beats Scissors"
+            }
+            if (computer === "paper") {
+                return "You Win! Scissors beats paper"
+            }
+    }
+}
+
+// console.log(playRound("RoCk", getComputerChoice()))
